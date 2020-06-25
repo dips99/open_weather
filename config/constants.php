4ddf29d10dbb1d1c9045d432b965e8f2d1f8b536
dips <dipanwita.das2008@gmail.com>
2020-06-25 11:17:36 +0530

    removed constantss

--- config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'open_weather' => [
-        'WEATHER_URL' => 'api.openweathermap.org/data/2.5/weather',
-        'WEATHER_APPID' => '516d1209944a8ef1922240867f2cb824'
-    ]
-];
